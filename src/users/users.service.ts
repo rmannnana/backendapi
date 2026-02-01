af3b4311d70e8ab38302bd3dcc96dcd500720721
B. Hermann NANA <borishermannana@gmail.com>
2026-02-01 16:45:16 +0000

    import userType into UsersService and creation of 5 users instances

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '../../types/usersType';
 
 @Injectable()
-export class UsersService { }
+export class UsersService {
+    private users: User[] = [
+        {
+            id: '1',
+            name: 'Hermann NANA',
+            email: 'hermann.nana@gmail.com',
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            id: '2',
+            name: 'Jane Smith',
+            email: 'jane.smith@example.com',
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            id: '3',
+            name: 'Alvin Jhonson',
+            email: 'alvin.jhonson@example.com',
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            id: '4',
+            name: 'Mohamed Sylla',
+            email: 'mohamed.sylla@example.com',
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            id: '5',
+            name: 'Agent DALTON',
+            email: 'agent.dalton@example.com',
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }
+    ];
+}
