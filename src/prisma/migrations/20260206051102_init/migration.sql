67ba42bdba4da001b22d3584d3c55d5b0888eb71
B. Hermann NANA <borishermannana@gmail.com>
2026-02-06 05:17:28 +0000

    I used npx prisma migrate dev --name init --schema ./src/prisma/schema.prisma --url postgresql://backendapi:backendpass@localhost:5432/mydb to make a new migration after deleteing container and recreating them wit docker-compose up

---
+++ src/prisma/migrations/20260206051102_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT,
+    "phone" TEXT,
+    "countryCode" TEXT,
+    "password" TEXT,
+    "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
